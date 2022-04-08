b3652c9d202c1eb3e33fc75aba559eeb05907f47
Rajeev Soni <sonirajeev021@gmail.com>
2022-04-08 13:58:22 +0530

    description of wallet app

--- src/main/java/com/rst/mywallet/exception/CustomerNotFoundException.java
+++ src/main/java/com/rst/mywallet/exception/CustomerNotFoundException.java
@@ -1,10 +1,28 @@
 package com.rst.mywallet.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class CustomerNotFoundException extends Exception{
 
 	private long customerId;
 	
 	public CustomerNotFoundException(long customerId) {
 		super(String.format("Customer is not found with id : '%s'", customerId));
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerNotFoundResponse> handleException(CustomerNotFoundResponse exc){
+		
+		CustomerNotFoundResponse error = new CustomerNotFoundResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage);
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
 }
